684a9e1a1292e2557037c17a8938645d3c97d6ce
Clefas <61330202+Clefas@users.noreply.github.com>
2023-01-04 15:40:05 -0300

    lockpick standalone

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/reset.css'
+}
